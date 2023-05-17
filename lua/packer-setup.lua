2d6d7ebc0af74d75348c5528814d9e64e415345c
real-codemeteor <86769666+real-codemeteor@users.noreply.github.com>
2023-05-17 14:21:50 +0200

    Update and rename packer.lua to packer-setup.lua

--- lua/packer-setup.lua
+++ lua/packer-setup.lua
@@ -1,4 +1,10 @@
 local packerPath = vim.fn.expand("~/.local/share/nvim/site/pack/packer/start/packer.nvim")
 if vim.fn.isdirectory(packerPath) == 0 then
+    print("Clone Packer")
     vim.fn.system("git clone --depth 1 https://github.com/wbthomason/packer.vim " .. packerPath)
 end
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+end)
