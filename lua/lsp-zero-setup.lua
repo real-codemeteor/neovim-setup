6d1b6b6afb6d1b3e4ee66248ab82b507675f18e6
real-codemeteor <86769666+real-codemeteor@users.noreply.github.com>
2023-07-28 15:00:23 +0200

    Update lsp-zero-setup.lua

--- lua/lsp-zero-setup.lua
+++ lua/lsp-zero-setup.lua
@@ -7,6 +7,9 @@ local lsp = require('lsp-zero').preset({
 
 lsp.setup()
 
+local null_ls = require('null-ls')
+local null_opts = lsp.build_options('null-ls', {})
+
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
@@ -14,4 +17,14 @@ vim.diagnostic.config({
   underline = true,
   severity_sort = false,
   float = true,
+})
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    null_opts.on_attach(client, bufnr)
+    --- you can add more stuff here if you need it
+  end,
+  sources = {
+    null_ls.builtins.formatting.black,
+  }
 })
