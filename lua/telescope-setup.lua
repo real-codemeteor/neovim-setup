924c9978efd9bef0deef88b0ed2c142cac591273
real-codemeteor <86769666+real-codemeteor@users.noreply.github.com>
2023-11-07 10:32:06 +0100

    Update telescope-setup.lua

--- lua/telescope-setup.lua
+++ lua/telescope-setup.lua
@@ -1,6 +1,6 @@
 telescope_setup = require("telescope")
 telescope_setup.load_extension "file_browser"
-telescope_setup.setup{
+telescope_setup.setup {
     extensions = {
         advanced_git_search = {
             diff_lugin = "fugitive"
@@ -9,3 +9,26 @@ telescope_setup.setup{
 }
 
 telescope_setup.load_extension("advanced_git_search")
+
+local action_state = require('telescope.actions.state')
+local actions = require('telescope.actions')
+
+local m = {}
+
+m.my_buffer = function()
+  require'telescope.builtin'.buffers{
+    attach_mappings = function(prompt_bufnr, map)
+      local delete_buf = function()
+        local selection = action_state.get_selected_entry()
+        actions.close(prompt_bufnr)
+        vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+      end
+
+      map('i', '<c-u>', delete_buf)
+
+      return true
+    end
+  }
+end
+
+return m
