7aedf2892694754b97b2811b92eadd2444224a1e
real-codemeteor <86769666+real-codemeteor@users.noreply.github.com>
2023-12-13 09:30:41 +0100

    Update packer-setup.lua

--- lua/packer-setup.lua
+++ lua/packer-setup.lua
@@ -74,7 +74,6 @@ return require('packer').startup(function(use)
     -- Install vimspector
     use 'puremourning/vimspector'
 
-
     -- Install treesitter
     use {
         'nvim-treesitter/nvim-treesitter'
@@ -84,4 +83,34 @@ return require('packer').startup(function(use)
 
     -- Install Codeium
     use 'Exafunction/codeium.vim'
+
+    use 'lukas-reineke/indent-blankline.nvim'
+
+    local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+    }
+
+    local hooks = require "ibl.hooks"
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    end)
+
+    require("ibl").setup { indent = { highlight = highlight } }
+
+
+    use 'nathanaelkane/vim-indent-guides'
 end)
